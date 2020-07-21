8075537a8010fc2ac9f8c5b5535cca0325478059
Björn Bebensee <bebensee@snu.ac.kr>
2020-07-21 16:48:49 +0900

    Added basic update on .json file change

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var jf = require('jsonfile'); //jsonfile module
+var fs = require('fs'); //require file system
+JSON_LOCATION = 'sample.json'
+
+EventEmitter = require('events').EventEmitter;
+jsonWatcher = new EventEmitter();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+  });;
+
+io.on('connection', (socket) => {
+    console.log('Client connected.');
+    
+    // Send initial status
+    jf.readFile(JSON_LOCATION, function(err, data) {
+        io.emit('jsonUpdate', data);
+    });
+
+    jsonWatcher.on('update', function(jsonData) {
+        console.log('Status update:', jsonData);
+        io.emit('jsonUpdate', jsonData);
+    });
+});
+
+// Look for changes in the file
+fs.watch(JSON_LOCATION, function(event, fileName) {
+    jf.readFile(JSON_LOCATION, function(err, data) { // upon change re-read the json file for updated status
+        jsonWatcher.emit('update', data)
+    });
 });
 
 http.listen(3000, () => {
